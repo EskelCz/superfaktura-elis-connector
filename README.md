470167e12e5e3445312d4999dad342b9553a6866
Eskel <spohr.david@gmail.com>
2019-05-06 16:15:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Superfaktura Elis Connector
+
+Export your invoice data from Rossum Elis to Superfaktura.cz expenses.
+
+### Installing
+
+1. Fill Superfaktura credentials and Elis secret code to connector.js, in credentials section
+2. Host a node.js server
+3. Deploy and run connector.js there (dont forget to install packages with yarn or npm)
+4. Add connector url to Elis (https://api.elis.rossum.ai/docs/#setup-a-connector)
+5. Profit :)
+
+# Example code for Elis
+Authenticate with terminal
+```
+curl -s -H 'Content-Type: application/json' \
+  -d '{"username": "...", "password": "..."}' \
+  'https://api.elis.rossum.ai/v1/auth/login'
+```
+Get queue list, so you know your queue IDs
+```
+curl -H 'Authorization: token 00e2818aba72e3e2a04810530b032531597dd392' \
+  'https://api.elis.rossum.ai/v1/queues?workspace=8194&ordering=name'
+```
+Add your deployed connector
+```
+curl -H 'Authorization: token 6e9fafc31dbbffb8479df1f622c0c3f5eb120931' -H 'Content-Type: application/json' \
+  -d '{"name": "SF Connector", "queues": ["https://api.elis.rossum.ai/v1/queues/####"], "service_url": "https://path-to-your-server.com", "authorization_token":"..."}' \
+  'https://api.elis.rossum.ai/v1/connectors'
+```
+
+# Schema
+Elis allows to you customize which data points are extracted and which are shown for human users for validation.
+This is distinguished by attibute `hidden: true|false`. For reference I've added the schema which I use, in example-schema.json.
+Read more on the topic here: https://api.elis.rossum.ai/docs/#document-schema
+
+# Notes
+
+Good stuff
+- The connector also sends the original pdf file as an attachment
+
+Missing stuff
+- Superfaktura doesn't allow adding of a total sum of VAT. They need sums of VAT categories, which currently is prone to mistakes from Elis and requires human validation. If you need to export VAT as well, feel free to contribute this functionality and let me know to issue #1.
---
+++ connector.js
@@ -0,0 +1,218 @@
+
+const express = require('express')
+const morgan = require('morgan')
+const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
+
+const app = express()
+const OPTIONAL = true
+
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+
+// Credentials
+
+const SFuser = ''
+const SFpassword = ''
+const ElisSecret = 'secret_key ...'
+
+// Helpers
+
+const findAllValues = (value, data) => {
+  if (data !== Object(data)) { return [] }
+  var results = []
+  if (data.constructor === Array) {
+    for (var i = 0, len = data.length; i < len; i++) { results = results.concat(findAllValues(value, data[i])) }
+    return results
+  }
+  for (var key in data) {
+    if (key === 'schema_id') {
+      if (data[key] === value) {
+        if (data['value']) { return data['value'] }
+      }
+    }
+    results = results.concat(findAllValues(value, data[key]))
+  }
+  return results
+}
+
+const findValue = (value, data, optional) => {
+  const listOfValues = findAllValues(value, data)
+  if (listOfValues.length > 0) {
+    return listOfValues[0]
+  } else if (!optional) {
+    throw Error('Missing value ' + value)
+  } else {
+    return null
+  }
+}
+
+const invoiceTypeTable = {
+  'tax_invoice': 'regular', // bežná daňová faktura
+  'debit_note': 'cancel', // námi vystavený dobropis, opravný daňový doklad, není evidován v nákladech
+  'credit_note': 'cancel', // námi přijatý dobropis, opravný daňový doklad
+  'proforma': 'proforma', // zálohová faktura
+  'other': 'draft' // jiné, koncept
+}
+
+const findInvoiceType = (elisType) => {
+  return invoiceTypeTable[elisType] ? invoiceTypeTable[elisType] : 'regular'
+}
+
+const sanitizeCurrency = (value) => {
+  if (typeof value === 'string') { return value.toUpperCase() }
+  return 'CZK'
+}
+
+const sanitizeNumber = (value) => {
+  if (value instanceof String) { return parseFloat(value.replace(/\s/g, '')) }
+  return value
+}
+
+const sanitizeString = (value) => {
+  if (typeof value === 'string') { return encodeURIComponent(value) }
+  return value
+}
+
+const getAccountPrefix = (number) => {
+  if (typeof number === 'string') {
+    const parts = number.split('-')
+    return parts.length > 1 ? parts[0] : null
+  } else {
+    return null
+  }
+}
+
+const getAccountNumber = (number) => {
+  if (typeof number === 'string') {
+    const parts = number.split('-')
+    if (parts.length > 1) {
+      return parts[1]
+    } else {
+      return parts[0]
+    }
+  } else {
+    return number
+  }
+}
+
+const sendError = (error, res) => {
+  console.log(error)
+  res.status(422).json({ 'messages': [{ 'content': error, 'type': 'error' }] })
+}
+
+const sendValidationWarning = (warning, res) => {
+  console.log(warning)
+  res.status(200).json({ 'messages': [{ 'content': warning, 'type': 'warning' }] })
+}
+
+// HTTP Auth
+
+app.use((req, res, next) => {
+  if (req.headers.authorization !== ElisSecret) {
+    res.set('WWW-Authenticate', 'Basic realm="401"')
+    res.status(401).send('Authentication required.')
+    return
+  }
+  next() // Access granted
+})
+
+app.get('/', (req, res) => res.send('The connector is running'))
+
+// Saving
+
+const parseData = (content, res, validation) => {
+  try {
+    return {
+      'Expense': {
+        'type': findInvoiceType(findValue('invoice_type', content)),
+        'name': sanitizeString(findValue('sender_name', content)),
+        'currency': sanitizeCurrency(findValue('currency', content)),
+        'variable': findValue('var_sym', content),
+        'amount': sanitizeNumber(findValue('amount_total', content)), // adding total amount to field for amount without tax, a workaround for SF
+        'vat': 0, // adding 0 instead of vat_detail_rate, to work around SF limitation of calculating total amount from tax
+        'due': findValue('date_due', content),
+        'created': findValue('date_issue', content),
+        'taxable_supply': null,
+        'payment_type': 'transfer',
+        'document_number': findValue('invoice_id', content, OPTIONAL),
+        'specific': findValue('spec_sym', content, OPTIONAL),
+        'constant': findValue('const_sym', content, OPTIONAL),
+        'delivery': findValue('date_uzp', content, OPTIONAL),
+        'comment': sanitizeString(findValue('notes', content, OPTIONAL))
+      },
+      'Client': {
+        'name': sanitizeString(findValue('sender_name', content)),
+        'ico': findValue('sender_ic', content),
+        'bank_account_prefix': getAccountPrefix(findValue('account_num', content, OPTIONAL)),
+        'bank_account': getAccountNumber(findValue('account_num', content)),
+        'bank_code': findValue('bank_num', content),
+        'dic': findValue('sender_dic', content, OPTIONAL),
+        'swift': findValue('bic', content, OPTIONAL),
+        'iban': findValue('iban', content, OPTIONAL),
+        'address': sanitizeString(findValue('sender_address', content, OPTIONAL))
+      }
+    }
+  } catch (err) {
+    if (validation) {
+      sendValidationWarning(err, res)
+    } else {
+      sendError(err, res)
+    }
+    return false
+  }
+}
+
+// Validation
+
+app.post('/validate', (req, res) => {
+  let data = req.body.content
+  const result = parseData(data, res, true)
+  if (result) {
+    res.json({ 'messages': [] }) // success response
+  }
+})
+
+// Saving
+
+app.post('/save', async (req, res) => {
+  const content = req.body.content
+  const data = parseData(content, res, false)
+
+  // get original document
+  const docPath = req.body.meta.original_file
+  const cleanPath = docPath.slice(8) // removing https://
+  const authPath = 'https://' + SFuser + ':' + SFpassword + '@' + cleanPath
+
+  try {
+    const fileRes = await fetch(authPath, { credentials: 'include' })
+    const buffer = await fileRes.buffer()
+    sendToSF(res, data, buffer.toString('base64'))
+  } catch (err) {
+    console.log('Original file not found', err)
+    sendToSF(res, data)
+  }
+})
+
+const sendToSF = (res, data, doc) => {
+  console.log('Sending data: ', data)
+  if (doc) { data['Expense'].attachment = doc }
+  fetch('https://moje.superfaktura.cz/expenses/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'Authorization': 'SFAPI email=spohrova%40seznam.cz&apikey=7b33981d56646810da778a156434ee5b&company_id=25295'
+    },
+    body: 'data=' + JSON.stringify(data)
+  })
+    .then((response) => {
+      response.json().then((result) => {
+        if (result.error === 1) { sendError(result.error_message, res) } else {
+          console.log('Success')
+          res.status(204).send() // success response
+        }
+      }).catch((error) => sendError(error, res))
+    }).catch((error) => sendError(error, res))
+}
+
+app.listen(process.env.PORT, () => console.log(`Connector listening on port ${process.env.PORT}`))
